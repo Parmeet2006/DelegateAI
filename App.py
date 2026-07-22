167c95f1b23ee7eeb403de5938edec8c2e358464
Parmeet2006 <parmeetbamrah054@gmail.com>
2026-07-22 12:19:20 +0530

    Inital changes

--- App.py
+++ App.py
@@ -1,4 +1,12 @@
-# Application File
+import json
+
+def save_contacts():
+    file=open('contacts.json','r')
+    contacts=file.read()
+    contacts_dictionary=json.load(contacts)
+    print(contacts_dictionary,type(contacts_dictionary))
+    contacts_to_save=contacts_dictionary['contacts']
+    
 
 def main():
     pass
